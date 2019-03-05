5618b800f8d64be20b868eef1e4c6a99ee737df9
zhaozhiwei <zhaozhiwei711@126.com>
2019-03-05 16:37:50 +0800

    update

--- spring-boot-2.x/spring-cloud-nacos/src/test/java/com/zzw/spring/cloud/SpringCloudNacosTest.java
+++ spring-boot-2.x/spring-cloud-nacos/src/test/java/com/zzw/spring/cloud/SpringCloudNacosTest.java
@@ -1,9 +1,14 @@
 package com.zzw.spring.cloud;
 
 import com.zzw.spring.cloud.nacos.SpringBootNacosApplication;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @author zhaozhiwei
@@ -12,4 +17,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootNacosApplication.class)
 public class SpringCloudNacosTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
 }
