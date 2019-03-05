e2d54c79d3282cdec462ecedf9dc0cfcfed0c3fe
zhaozhiwei <zhaozhiwei711@126.com>
2019-03-06 00:28:00 +0800

    update

---
+++ java-basic/src/main/java/com/zzw/trta/generic/BasicBounds.java
@@ -0,0 +1,118 @@
+package com.zzw.trta.generic;
+
+import java.awt.*;
+
+/**
+ * @author zhaozhiwei
+ * @date 2019/3/5 23:06
+ */
+public class BasicBounds {
+
+    public static void main(String[] args) {
+        Solid<Bounded> solid = new Solid<>(new Bounded());
+        solid.color();
+        solid.getX();
+        solid.weight();
+    }
+}
+
+interface HasColor {
+    Color getColor();
+}
+
+class Colored<T extends HasColor> {
+    T item;
+
+    public Colored(T item) {
+        this.item = item;
+    }
+
+    T getItem() {
+        return item;
+    }
+
+    Color color() {
+        return item.getColor();
+    }
+}
+
+class Dimension {
+    public int x, y, z;
+}
+
+class ColoredDimension<T extends Dimension & HasColor> {
+    T item;
+
+    public ColoredDimension(T item) {
+        this.item = item;
+    }
+
+    T getItem() {
+        return item;
+    }
+
+    Color color() {
+        return item.getColor();
+    }
+
+    int getX() {
+        return item.x;
+    }
+
+    int getY() {
+        return item.y;
+    }
+
+    int getZ() {
+        return item.z;
+    }
+}
+
+interface Weight {
+    int weight();
+}
+
+class Solid<T extends Dimension & Weight & HasColor> {
+    T item;
+
+    public Solid(T item) {
+        this.item = item;
+    }
+
+    T getItem() {
+        return item;
+    }
+
+    Color color() {
+        return item.getColor();
+    }
+
+    int getX() {
+        return item.x;
+    }
+
+    int getY() {
+        return item.y;
+    }
+
+    int getZ() {
+        return item.z;
+    }
+
+    int weight() {
+        return item.weight();
+    }
+}
+
+class Bounded extends Dimension implements HasColor, Weight {
+
+    @Override
+    public Color getColor() {
+        return null;
+    }
+
+    @Override
+    public int weight() {
+        return 0;
+    }
+}
