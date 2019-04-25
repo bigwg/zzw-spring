912da560b175b59fe02c67b22cf083eed9fecc74
zhaozhiwei <zhaozhiwei711@126.com>
2019-04-25 13:32:58 +0800

    update

--- spring-boot-2.x/spring-boot-base-2.x/src/main/java/com/zzw/spring/boot/base/event/SaySomethingListener.java
+++ spring-boot-2.x/spring-boot-base-2.x/src/main/java/com/zzw/spring/boot/base/event/SaySomethingListener.java
@@ -1,8 +1,6 @@
 package com.zzw.spring.boot.base.event;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +10,15 @@ import org.springframework.stereotype.Service;
  * @author zhaozhiwei
  * @date 2019/4/22 0:08
  */
+@Slf4j
 @Service
 public class SaySomethingListener {
 
     @EventListener(value = SaySomethingEvent.class)
     public void onApplicationEvent(SaySomethingEvent event) {
-        System.out.println(event.getMessage());
+        log.error(event.getMessage());
+        log.warn(event.getMessage());
+        log.info(event.getMessage());
     }
 
 }
