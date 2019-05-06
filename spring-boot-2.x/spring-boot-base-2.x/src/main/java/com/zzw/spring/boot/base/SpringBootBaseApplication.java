f745c84acd303ed53ec9329d1486051845cbd267
zhaozhiwei <zhaozhiwei711@126.com>
2019-05-06 13:47:33 +0800

    update

--- spring-boot-2.x/spring-boot-base-2.x/src/main/java/com/zzw/spring/boot/base/SpringBootBaseApplication.java
+++ spring-boot-2.x/spring-boot-base-2.x/src/main/java/com/zzw/spring/boot/base/SpringBootBaseApplication.java
@@ -1,9 +1,11 @@
 package com.zzw.spring.boot.base;
 
 import com.zzw.spring.boot.base.event.SaySomethingEvent;
+import com.zzw.spring.boot.base.service.Waiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
@@ -15,6 +17,7 @@ import java.math.BigDecimal;
  * @date 2018/9/24 上午12:00
  */
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @ImportResource(locations = {
         "classpath:META-INF/spring/context.xml",
         "classpath:META-INF/spring/context-test.xml"}
@@ -25,5 +28,8 @@ public class SpringBootBaseApplication {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootBaseApplication.class, args);
         SaySomethingEvent event = new SaySomethingEvent(applicationContext, "赵志伟，你好！");
         applicationContext.publishEvent(event);
+        Waiter naiveWaiter = applicationContext.getBean("naiveWaiter", Waiter.class);
+        naiveWaiter.greetTo("赵志伟");
+        System.out.println(naiveWaiter.getClass().getSimpleName());
     }
 }
